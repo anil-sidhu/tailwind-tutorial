adc38a0d680866a1e25693cc363e944a2b5e8917
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-09-10 22:26:08 +0530

    function done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,7 @@ module.exports = {
   theme: {
 
     extend: {
-      spacing: {
-        '21': '6rem',
-        '22': '3rem',
-        '13':'13px',
-        'px-8':'8px',
-        'px-100':'100px'
-      }
+
     },
     container: {
       center: true
